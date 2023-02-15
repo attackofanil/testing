2e5c910ec6b31c493ecb439aeac616033868991a
Саша Березин <attackofsnail@gmail.com>
2023-02-15 12:38:01 +0300

    all

---
+++ tests/test_capitalize.py
@@ -0,0 +1,9 @@
+from testing.scr.capitalize import capitalize
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
